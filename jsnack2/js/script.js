b47f27a5223670c3c5bd8390ce8d86dd3e140fd3
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-11 15:18:47 +0100

    init jsnack 2

---
+++ jsnack2/js/script.js
@@ -0,0 +1,14 @@
+console.log('JS OK!');
+
+// Il software chiede per 5 volte all’utente di inserire un numero
+// Il programma stampa la somma di tutti i numeri inseriti
+// Esegui questo programma in due versioni, con for e con while
+
+
+let userNumbers;
+
+sum = 0;
+
+for (let i = 0; 1 <= 5; i++) {
+    userNumbers = parseInt(prompt('Inserisci un numero'));
+}
