034289fdff7054af020c7a77177849312ae39273
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-11 15:11:59 +0100

    jsnack 1 completato

---
+++ jsnack1/js/script.js
@@ -0,0 +1,14 @@
+console.log('JS OK!');
+
+// Crea un array di numeri interi
+//somma tutti gli elementi che sono in posizione dispari
+
+const arrNumeri = [23, 21, 7, 89, 77, 62, 32];
+
+arrNumeri.length;
+
+somma = 0;
+
+for (let i = 0; i < arrNumeri.length; i = i + 2) {
+    console.log(arrNumeri[i])
+}
